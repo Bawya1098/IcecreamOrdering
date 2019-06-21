7afa7800cbdff52b9fbb351968495f0fa0fef006
intern <bawya1098@gmail.com>
2019-06-21 12:24:22 +0530

    Displayed menu with ice cream types and flavours and removed unncessary items

--- src/com/company/menu.java
+++ src/com/company/menu.java
@@ -1,55 +1,25 @@
 package com.company;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 class menu {
 
-    private ArrayList<Integer> sno = new ArrayList<>() {{
-        add(1);
-        add(2);
-        add(3);
-    }};
-    private ArrayList<String> type = new ArrayList<>() {{
-        add("STICK");
-        add("CONE");
-        add("CUP");
-    }};
-    private ArrayList<Integer> price = new ArrayList<>() {{
-        add(25);
-        add(45);
-        add(15);
-    }};
-    private ArrayList<String> flavours = new ArrayList<>() {{
-        add("CHOCOLATE");
-        add("VANILLA");
-        add("STRAWBERRY");
-    }};
-    private ArrayList<Integer> flavours_cost = new ArrayList<>() {{
-        add(20);
-        add(25);
-        add(35);
-    }};
-    private ArrayList<String> topping = new ArrayList<>() {{
-        add("CHOCOLATE");
-        add("STRAWBERRY");
-        add("VANILLA");
-    }};
-    private ArrayList<Integer> additional_cost = new ArrayList<>() {{
-        add(25);
-        add(20);
-        add(35);
-    }};
+    private List<Integer> sno = Arrays.asList(1, 2, 3);
+
+    private List<String> type = Arrays.asList("STICK", "CONE", "CUP");
+
+    private List<String> flavours = Arrays.asList("CHOCOLATE", "VANILLA", "STRAWBERRY");
 
     void menuDisplay() {
-        System.out.println("------------------------------------------------------------------------------------------------------------------------------------");
-        System.out.printf("%10s %10s %10s %20s %20s %20s %20S", "SERIALNO", "ICECREAM_TYPE", "TYPE_COST", "FLAVORS", "FLAVOURS_COST", "TOPPING", "ADDITIONALCOST");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.printf("%10s %10s  %20s ", "SERIALNO", "ICECREAM_TYPE", "FLAVORS");
         System.out.println();
-        System.out.println("-------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         for (int i = 0; i < sno.size(); i++) {
-            System.out.printf("%10s %10s %10s %20s %20s %20S %20s", sno.get(i), type.get(i), price.get(i), flavours.get(i), flavours_cost.get(i), topping.get(i), additional_cost.get(i));
+            System.out.printf("%5s %10s %25s  ", sno.get(i), type.get(i), flavours.get(i));
             System.out.println();
         }
     }
-
 }
 
