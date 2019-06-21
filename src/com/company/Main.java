807f3b00b920af08ca0000e87b6846b7160b6fcd
intern <bawya1098@gmail.com>
2019-06-21 13:29:45 +0530

    displayed Ice Creams

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,13 @@
 package com.company;
 
-import java.util.Scanner;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
+        Stall stall = new Stall();
+        List<IceCream> iceCreams = stall.createIceCream();
         menu menu = new menu();
-        menu.menuDisplay();
-        Scanner sc = new Scanner(System.in);
-
+        menu.display(iceCreams);
     }
 }
--- src/com/company/menu.java
+++ src/com/company/menu.java
@@ -1,23 +1,18 @@
 package com.company;
 
-import java.util.Arrays;
 import java.util.List;
 
 class menu {
-
-    private List<Integer> sno = Arrays.asList(1, 2, 3);
-
-    private List<String> type = Arrays.asList("STICK", "CONE", "CUP");
-
-    private List<String> flavours = Arrays.asList("CHOCOLATE", "VANILLA", "STRAWBERRY");
-
-    void menuDisplay() {
-        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        System.out.printf("%10s %10s  %20s ", "SERIALNO", "ICECREAM_TYPE", "FLAVORS");
+    void display(List<IceCream> iceCreams) {
+        System.out.printf("%45s", "MENU");
         System.out.println();
-        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        for (int i = 0; i < sno.size(); i++) {
-            System.out.printf("%5s %10s %25s  ", sno.get(i), type.get(i), flavours.get(i));
+        System.out.println("\t\t\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.printf("%20s %30s", "s.no", "ICE CREAM");
+        System.out.println();
+        System.out.println("\t\t\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        for (int i = 0; i < iceCreams.size(); i++) {
+            IceCream ice = iceCreams.get(i);
+            System.out.printf("%20s %30s ", i + 1, ice.toString());
             System.out.println();
         }
     }
