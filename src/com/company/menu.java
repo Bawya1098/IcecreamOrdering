98c75bef69f82e1e7699f7374b8eda5d9eb3d57e
intern <bawya1098@gmail.com>
2019-06-24 11:18:40 +0530

    fixed menu class properties

--- src/com/company/menu.java
+++ src/com/company/menu.java
@@ -1,17 +1,24 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class menu {
-    void showIceCreamWithCost(List<IceCream> iceCreams) {
+    private List<IceCream> iceCreamList = new ArrayList<>();
+
+    void addIcecream(List<IceCream> iceCreams) {
+        iceCreamList.addAll(iceCreams);
+    }
+
+    void showIceCreamWithCost() {
         System.out.printf("%45s", "MENU");
         System.out.println();
         System.out.println("\t\t\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         System.out.printf("%20s %30s %10s", "s.no", "ICE CREAM", "Cost");
         System.out.println();
         System.out.println("\t\t\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        for (int i = 0; i < iceCreams.size(); i++) {
-            IceCream ice = iceCreams.get(i);
+        for (int i = 0; i < iceCreamList.size(); i++) {
+            IceCream ice = iceCreamList.get(i);
             System.out.printf("%20s %30s ", i + 1, ice.toString());
             System.out.println();
         }
