925ce887465f463d5e89c93324b31a9036e68b89
intern <bawya1098@gmail.com>
2019-06-24 11:20:09 +0530

    fixed quantity and displayed totalcost

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,15 +8,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Stall stall = new Stall();
-        List<IceCream> iceCreams = stall.createIceCream();
         menu menu = new menu();
-        menu.showIceCreamWithCost(iceCreams);
+        List<IceCream> iceCreams = stall.createIceCream();
+        menu.addIcecream(iceCreams);
+        menu.showIceCreamWithCost();
         System.out.println("Enter what you wanna Try out: ");
         String userIceCream = scan.nextLine();
         System.out.println("Enter Quantity: ");
-        int quantity = scan.nextInt();
-        int cost = stall.calculatecost(userIceCream);
+        int quantity = Integer.parseInt(scan.nextLine());
+        int cost = stall.calculateCost(userIceCream, iceCreams, scan);
         int totalCost = cost * quantity;
-        System.out.println(totalCost);
+        System.out.println("cost : " + totalCost);
     }
 }
